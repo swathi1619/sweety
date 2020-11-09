532d80bb97ae6339f4480149e54b674e2e49395c
swathi1619 <73834034+swathi1619@users.noreply.github.com>
2020-11-09 19:19:14 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,34 +1,51 @@
-package src;
+package swathi;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-/**
-	 *Definition for an interval.
-	 **public class Interval {
-	 **int start;
-	 **int end;
-	 **Interval() { start = 0; end = 0; }
-	 **Interval(int s, int e) { start = s; end = e; }
-	 *}
-	 */
-	public class Solution {
-		public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-			ArrayList<Interval> result = new ArrayList<Interval>();
-			for(Interval interval: intervals){
-				if(interval.start < newInterval.start){
-					result.add(interval);
-					}else if(interval.start > newInterval.start){
-						result.add(newInterval);
-						newInterval = interval;
-						}
-					else if(interval.start >= newInterval.start || interval.start <=newInterval.start){
-						newInterval = new Interval();
-						}
-			}
-			result.add(newInterval);
+public class Solution {
+	public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		if (num.length < 3)
 			return result;
+		// sort array
+		Arrays.sort(num);
+		for (int i = 0; i < num.length - 2; i++) {
+			// //avoid duplicate solutions
+			if (i == 0 || num[i] > num[i - 1]) {
+				int negate = -num[i];
+				int start = i + 1;
+				int end = num.length - 1;
+				while (start < end) {
+					//case 1
+					if (num[start] + num[end] == negate) {
+						ArrayList<Integer> temp = new ArrayList<Integer>();
+						temp.add(num[i]);
+						temp.add(num[start]);
+						temp.add(num[end]);
+						result.add(temp);
+						start++;
+						end--;
+						//avoid duplicate solutions
+						while (start < end && num[end] == num[end + 1])
+								end--;
+						while (start < end && num[start] == num[start - 1])
+							start++;
+						//case 2
+						}
+					else if (num[start] + num[end] < negate) {
+						start++;
+						//case 3
+						} 
+					else {
+						end--;
+						}
+					}
+				}	
 			}
-		}
+		
+		return result;
+	}
 
-
+}
 
